63d26f43e94861e0636d86ca38b8c651cd5af3ba
Alex Burrows <burrows.alexj@gmail.com>
2024-12-01 18:37:46 -0500

    Add status section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header"
+import Status from "./components/Status"
 
 export default function App(){
   return (
     <main>
       <Header />
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>Well done!</p>
+      </section>
     </main>
   )
 }
---
+++ src/components/Status.jsx
@@ -0,0 +1,8 @@
+export default function Status() {
+    return (
+        <section className="status-container">
+            <h1>You Win!</h1>
+            <p>Well done!</p>
+        </section>
+    )
+}
