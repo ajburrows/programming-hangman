1189eaa4c7b60b71a1508224997f6a71afce175b
Alex Burrows <burrows.alexj@gmail.com>
2024-12-01 20:44:14 -0500

    Set key colors when guessed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import Header from "./components/Header"
 import LanguageChip from "./components/LanguageChip"
 import { languages } from "./languages"
 import { useState } from "react"
+import { clsx } from "clsx"
 
 export default function App(){
   const [currentWord, setCurrentWord] = useState("react")
+  const [guessedLetters, setGuessedLetters] = useState([])
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+  function addGuessedLetter(letter) {
+    setGuessedLetters(prevGuessedLetters => {
+      const letterSet = new Set(prevGuessedLetters)
+      letterSet.add(letter)
+      return Array.from(letterSet)
+    })
+  }
   const languageChips = languages.map(langObj => 
     <LanguageChip 
       key={langObj.name}
@@ -16,14 +25,31 @@ export default function App(){
     />
   )
 
-  console.log(currentWord)
+  console.log(guessedLetters)
   const letterSpans = currentWord.split("").map((letter, index) => 
     <span className="letter" key={index}>{letter.toUpperCase()}</span>
   )
 
-  const keyboardButtons = alphabet.split("").map(letter => 
-    <button className="keyboard-button" key={letter.toUpperCase()} onClick={() => console.log(letter.toUpperCase())}>{letter.toUpperCase()}</button>
-  )
+  const keyboardButtons = alphabet.split("").map(letter => {
+    const isGuessed = guessedLetters.includes(letter.toUpperCase())
+    const isRight = isGuessed && currentWord.includes(letter)
+    const isWrong = isGuessed && !currentWord.includes(letter)
+    const className = clsx({
+      "keyboard-button": true,
+      "right-letter": isRight,
+      "wrong-letter": isWrong,
+    })
+
+    return(
+      <button 
+        className={className}
+        key={letter.toUpperCase()} 
+        onClick={() => addGuessedLetter(letter.toUpperCase())}
+      >
+          {letter.toUpperCase()}
+      </button>
+    )
+  })
 
   return (
     <main>
@@ -41,6 +67,7 @@ export default function App(){
       <section className="keyboard">
         {keyboardButtons}
       </section>
+      <button className="new-game">New Game</button>
     </main>
   )
 }
